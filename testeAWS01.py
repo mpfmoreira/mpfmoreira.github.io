476e4de60a0c0c526cd9f1fd208272ce405a1f7f
mpfmoreira <69805927+mpfmoreira@users.noreply.github.com>
2023-07-23 02:48:41 +0000

    Update testeAWS01.py

--- testeAWS01.py
+++ testeAWS01.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
 import openpyxl
@@ -44,19 +45,13 @@ def lerSite():
 
 if 5 > 6 :
     source = lerSite()
-    ArqTeste = open("https://pactum-gaveta.s3.us-east-2.amazonaws.com/teste5.html", "r")
-
-    ArqTeste.write(source)
-    ArqTeste.close()
     lista = pd.read_html(source,thousands=None)
-
-
 else:
-### na lista estão todas as planilhas do arquivo. Neste caso, é só uma
     lista = pd.read_html('https://pactum-gaveta.s3.us-east-2.amazonaws.com/teste5.html',thousands=None)
-
+### na lista estão todas as planilhas do arquivo. Neste caso, é só uma
 ###  df é a planilha que foi criada pelo sistema
 df = lista[0]
+st.write(lista)
 ###  troca pontos e vírgulas. Acrescenta cabeçalho que está em branco
 df["Valor"] = df["Valor"].str.replace(".","",regex=False)
 df["Valor"] = df["Valor"].str.replace(",",".",regex=True)
@@ -108,24 +103,5 @@ for i in range(1,x.size):
 ### ordena df2
 df2 = df2.sort_index().reset_index(drop=True)
 
-### Grava e coloca formato em Valor e em Data
-#df2.to_excel('testeReverso1.xlsx')
-gravador = pd.ExcelWriter('~/testeReverso6.xlsx',engine='xlsxwriter')
-#gravador = pd.ExcelWriter('/Users/mpedro/Downloads/testeReverso6.xlsx',engine='xlsxwriter')
-df2.to_excel(gravador, sheet_name='Movimento')
-arquivo=gravador.book
-plan=gravador.sheets['Movimento']
-formato_valor=arquivo.add_format({'num_format': '#,##0.00'})
-formato_data=arquivo.add_format({'num_format': 'dd/mm/yy'})
-plan.set_column(1,1,10,formato_data)
-plan.set_column(5,5,14,formato_valor)
-#gravador.close()
-
-pivot = np.round(pd.pivot_table(df2, values='Valor',index=['PC','a','b','c']
-                                ,columns='CR',aggfunc=np.sum),2)
-plan2 = arquivo.add_worksheet('Tabela')
-pivot.to_excel(gravador, sheet_name='Tabela')
-
-gravador.close()
 
 print(" FIM ")
